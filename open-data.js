8beb83c97d8411cf9da67bb286d10dac6164f96d
esneyenkus <kadircanaybakir@gmail.com>
2026-08-15 01:21:31 +0300

    Add files via upload

--- open-data.js
+++ open-data.js
@@ -34,9 +34,7 @@ const WMS_CONFIGS = [
     bounds: [39.845453, 25.412765, 42.261958, 30.364822],
     provider: 'Coğrafi Bilgi Sistemleri Genel Müdürlüğü',
     sourceUrl: `${ULASAV_ROOT}/dataset/?q=${encodeURIComponent('Tekirdağ Kırklareli Edirne Çevre Düzeni Planı')}`,
-    description: 'Trakya planlama bölgesindeki üst ölçekli arazi kullanım kararlarını gösterir.',
-    preferCompositeLayers: true,
-    choiceRevision: 'stable-viewport-composite-2'
+    description: 'Trakya planlama bölgesindeki üst ölçekli arazi kullanım kararlarını gösterir.'
   },
   {
     key: 'cdp-kirikkale',
